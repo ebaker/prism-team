33007472c7d977b2500efbc068ff8c3dc9c84c1e
Eliot Baker <eliotbaker@gmail.com>
2015-07-14 13:44:24 -0400

    added cheerio html parser. parsed html finding name and role

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+var $ = require('cheerio');
 var request = require('request');
 
 var URL = 'https://prism.com/about/';
 
 request(URL, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the about
+    var parsedHTML = $.load(body);
+
+    // get all li tags within ul.img-container and loop over them
+    var imageURLs = [];
+    parsedHTML('ul.img-container li').map(function(i, li) {
+      var name = $(li).find('.caption > .bold').text();
+      var role = $(li).find('.caption > .lead').text();
+      // console.log('%s: %s, %s', i, name, role);
+      console.log('%s, %s', name, role);
+    }); 
   }
 });
