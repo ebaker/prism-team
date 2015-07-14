123a058704fbf312fc10bee2f9602397179e24b0
Eliot Baker <eliotbaker@gmail.com>
2015-07-14 13:56:20 -0400

    more comments

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-var $ = require('cheerio');
-var request = require('request');
+var request = require('request'); // HTTP client
+var $ = require('cheerio'); // HTML parser
 
 var URL = 'https://prism.com/about/';
 
+// perform GET request on URL
 request(URL, function (error, response, body) {
+  
+  // upon success (error code 200) and no errors
   if (!error && response.statusCode == 200) {
+
+    // parse HTML via cheerio
     var parsedHTML = $.load(body);
 
     // get all li tags within ul.img-container and loop over them
-    var imageURLs = [];
     parsedHTML('ul.img-container li').map(function(i, li) {
+
+      // extract name stored in dom element class 'bold'
       var name = $(li).find('.caption > .bold').text();
+      
+      // extract name stored in dom element class 'lead'
       var role = $(li).find('.caption > .lead').text();
-      // console.log('%s: %s, %s', i, name, role);
+
+      // output data in "<name>, <role>" format
       console.log('%s, %s', name, role);
     }); 
   }
